9e5648fe4c67f1c6168d0f9c507891ddbb80f654
paollooo <gguoghhvo@gmail.com>
2024-06-12 15:57:16 +0300

    bbb

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/maksirh/db-labs-IO24-team3 master:gh-pages
+git push -f https://github.com/paollooo/db-labs-IO24-team3.git master:gh-pages
 
 cd -
